ece5d2df8355de40f6d0a9a15e3fb5c38ee34ade
Deni Cho <denichodev@gmail.com>
2019-08-31 17:09:06 +0700

    Init dashboard

---
+++ src/components/layout/MainSidebar/SidebarNavItems.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Nav } from "shards-react";
+
+import SidebarContext from 'contexts/SidebarContext';
+
+import SidebarNavItem from "./SidebarNavItem";
+
+const SidebarNavItems = () => {
+  const sidebarData = React.useContext(SidebarContext);
+
+  return (
+          <div className="nav-wrapper">
+            <Nav className="nav--no-borders flex-column">
+              {sidebarData.items.map((item, idx) => (
+                <SidebarNavItem key={idx} item={item} />
+              ))}
+            </Nav>
+          </div>
+        )
+}
+
+// class SidebarNavItems extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       navItems: Store.getSidebarItems()
+//     };
+
+//     this.onChange = this.onChange.bind(this);
+//   }
+
+//   componentWillMount() {
+//     Store.addChangeListener(this.onChange);
+//   }
+
+//   componentWillUnmount() {
+//     Store.removeChangeListener(this.onChange);
+//   }
+
+//   onChange() {
+//     this.setState({
+//       ...this.state,
+//       navItems: Store.getSidebarItems()
+//     });
+//   }
+
+//   render() {
+//     const { navItems: items } = this.state;
+//     return (
+//       <div className="nav-wrapper">
+//         <Nav className="nav--no-borders flex-column">
+//           {items.map((item, idx) => (
+//             <SidebarNavItem key={idx} item={item} />
+//           ))}
+//         </Nav>
+//       </div>
+//     )
+//   }
+// }
+
+export default SidebarNavItems;
