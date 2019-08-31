20250dbe0ef88391213bfb87dda3bde8c0d80257
Deni Cho <denichodev@gmail.com>
2019-08-31 18:18:19 +0700

    User auth check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import SidebarContext, {defaultValue} from 'contexts/SidebarContext';
+import {UserProvider} from 'contexts/UserContext';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/shards-dashboards.1.1.0.min.css"
@@ -7,23 +9,29 @@ import "./assets/shards-dashboards.1.1.0.min.css"
 import routes from "./routes";
 
 export default () => (
-  <Router basename={process.env.REACT_APP_BASENAME || ""}>
-    <div>
-      {routes.map((route, index) => {
-        return (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={props => (
-                <route.layout {...props}>
-                  <route.component {...props} />
-                </route.layout>
-              )
-            }
-          />
-        );
-      })}
-    </div>
-  </Router>
+    <SidebarContext.Provider value={defaultValue}>
+      <Router basename={process.env.REACT_APP_BASENAME || ""}>
+  <UserProvider>
+
+        <div>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={props => (
+                    <route.layout {...props}>
+                      <route.component {...props} />
+                    </route.layout>
+                  )
+                }
+              />
+            );
+          })}
+        </div>
+  </UserProvider>
+
+      </Router>
+    </SidebarContext.Provider>
 );
